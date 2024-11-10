2289771849377116a93cd8e4327409aff0dfb912
Andrii Dovhal <dovhaland@gmail.com>
2024-11-10 18:00:15 +0100

    Changed CORS. 10.11.2024 18:00.

--- backend/spring-server-v1/src/main/java/com/dovhal/application/model/Review.java
+++ backend/spring-server-v1/src/main/java/com/dovhal/application/model/Review.java
@@ -3,7 +3,7 @@ package com.dovhal.application.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.Data;
+// import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import java.time.LocalDateTime;
