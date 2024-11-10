df394778d624f0b5e88512d8106c526bb139ddd1
Andrii Dovhal <dovhaland@gmail.com>
2024-11-10 19:14:36 +0100

    Resolved JoinDate in Profile.

--- backend/spring-server-v1/src/main/java/com/dovhal/application/controllers/UserController.java
+++ backend/spring-server-v1/src/main/java/com/dovhal/application/controllers/UserController.java
@@ -106,7 +106,7 @@ public class UserController {
             Map<String, Object> response = new HashMap<>();
             response.put("id", user.getId());
             response.put("email", user.getEmail());
-            response.put("dob", user.getJoinDate());
+            response.put("joinDate", user.getJoinDate());
             response.put("name", user.getName());
             response.put("fullName", user.getName() + " " + user.getSurname());
 
