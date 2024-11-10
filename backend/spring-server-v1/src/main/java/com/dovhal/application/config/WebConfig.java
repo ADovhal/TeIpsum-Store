8f109031f2451f5fd5ed098502294e9724ad31d0
Andrii Dovhal <dovhaland@gmail.com>
2024-11-10 18:54:18 +0100

    Changed CORS.

--- backend/spring-server-v1/src/main/java/com/dovhal/application/config/WebConfig.java
+++ backend/spring-server-v1/src/main/java/com/dovhal/application/config/WebConfig.java
@@ -16,8 +16,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                // .allowedOrigins("https://andriidovhal.tech/", "https://resume.vladyslavdobrovolskyi.tech/", "https://app.andriidovhal.tech/")
                 .allowedOrigins(allowedOriginsS.split(","))
-                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
