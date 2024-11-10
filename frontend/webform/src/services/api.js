f10c65cef24e539fea144084bf3f4aef94f80547
Andrii Dovhal <dovhaland@gmail.com>
2024-11-10 17:24:42 +0100

    Now should work! Corrected mistake in url variable name!

--- frontend/webform/src/services/api.js
+++ frontend/webform/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_BASE_URL, // Устанавливаем базовый URL для всех запросов
+    baseURL: process.env.REACT_APP_API_URL_TEST, // Устанавливаем базовый URL для всех запросов
     headers: {
         'Content-Type': 'application/json',
     },
