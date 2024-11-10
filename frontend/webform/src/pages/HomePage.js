8fe44660ccda22af1703a3e5a065cd78092336f5
Andrii Dovhal <dovhaland@gmail.com>
2024-11-10 16:56:22 +0100

    Trying to fix

--- frontend/webform/src/pages/HomePage.js
+++ frontend/webform/src/pages/HomePage.js
@@ -10,6 +10,8 @@ const HomePage = () => {
     document.title = "My Store Home Page";
   }, []);
 
+  console.log(process.env.REACT_APP_API_BASE_URL);
+
   const popularProducts = [
     {
       id: 1,
