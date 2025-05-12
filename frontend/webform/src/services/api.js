814792f884af5ad30889c74ddef9c45092edf00c
Andrii Dovhal <dovhaland@gmail.com>
2025-05-12 18:49:55 +0200

    Refactor project structure: remove unused docker-compose files, update nginx configurations, and adjust API base URL for consistency.

--- frontend/webform/src/services/api.js
+++ frontend/webform/src/services/api.js
@@ -3,7 +3,7 @@ import store from '../redux/store';
 import { refreshToken, logoutAsync } from '../features/auth/authSlice';
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL_TEST,
+    baseURL: process.env.REACT_APP_API_URL,
     headers: {
         'Content-Type': 'application/json',
     },
