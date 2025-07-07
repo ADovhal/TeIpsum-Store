602cb1b7530db97501d8508ffb176b4b883e4f1a
Andrii Dovhal <dovhaland@gmail.com>
2025-07-07 18:00:46 +0200

    Add new dependencies and update routing structure in App component

--- frontend/webform/src/App.js
+++ frontend/webform/src/App.js
@@ -1,21 +1,22 @@
+// src/App.js
 import React, { useEffect } from 'react';
 import GlobalStyles from './styles/GlobalStyles';
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import HomePage from './pages/HomePage';
-import StorePage from './pages/StorePage'
-import LoginPage from './pages/LoginPage'
-import RegisterPage from './pages/RegisterPage'
+import HomePage from './pages/homePage/HomePage';
+import AboutPage from './pages/aboutPage/AboutPage';
+import StorePage from './pages/StorePage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 import ProfilePage from './pages/ProfilePage';
 import PrivateRoute from './routes/PrivateRoute';
 import {ViewTypeProvider} from './context/ViewTypeContext';
 import { useDispatch, useSelector } from 'react-redux';
 import { loadProfile } from './features/profile/profileSlice';
-
+import { HeaderHeightProvider } from './context/HeaderHeightContext.js';
 
 const App = () => {
-
     const dispatch = useDispatch();
     const profileData = useSelector((state) => state.profile.profileData);
     const accessToken = useSelector((state) => state.auth.accessToken);
@@ -29,19 +30,22 @@ const App = () => {
     return (
         <>
             <GlobalStyles />
-            <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-                <Header />
-                <div style={{ flex: '1' }}>
-                    <Routes>
-                        <Route path="/" element={<HomePage />} />
-                        <Route path="/store" element={<ViewTypeProvider> <StorePage /> </ViewTypeProvider>} />
-                        <Route path="/register" element={<RegisterPage />} />
-                        <Route path="/login" element={<LoginPage />} />
-                        <Route path="/profile" element={<PrivateRoute element={ProfilePage} />} />
-                    </Routes>
+            <HeaderHeightProvider>
+                <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+                    <Header />
+                    <div style={{ flex: '1' }}>
+                        <Routes>
+                            <Route path="/" element={<HomePage />} />
+                            <Route path="/about" element={<AboutPage />} />
+                            <Route path="/store" element={<ViewTypeProvider> <StorePage /> </ViewTypeProvider>} />
+                            <Route path="/register" element={<RegisterPage />} />
+                            <Route path="/login" element={<LoginPage />} />
+                            <Route path="/profile" element={<PrivateRoute element={ProfilePage} />} />
+                        </Routes>
+                    </div>
+                    <Footer />
                 </div>
-                <Footer />
-            </div>
+            </HeaderHeightProvider>
         </>
     );
 }
