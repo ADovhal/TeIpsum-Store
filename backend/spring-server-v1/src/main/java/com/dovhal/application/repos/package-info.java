b7b56bc489a4baba64c09b7dff0098e8c8106171
Andrii Dovhal <dovhaland@gmail.com>
2025-07-20 00:41:05 +0200

    Remove backend and frontend components for the spring-server-v1 project, including all related configurations, models, controllers, services, repositories, and properties files. This commit effectively deletes the entire project structure, including Docker and Nginx configurations.

--- backend/spring-server-v1/src/main/java/com/dovhal/application/repos/package-info.java
+++
@@ -1,4 +0,0 @@
-@NonNullApi
-package com.dovhal.application.repos;
-
-import org.springframework.lang.NonNullApi;
--- backend/spring-server-v1/src/main/java/com/dovhal/application/security/package-info.java
+++
@@ -1,4 +0,0 @@
-@NonNullApi
-package com.dovhal.application.security;
-
-import org.springframework.lang.NonNullApi;
