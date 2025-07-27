a6adca2da13ad0e0adc0cb49d19af6e8bcc3a76d
Andrii Dovhal <dovhaland@gmail.com>
2025-07-27 03:30:00 +0200

    Resolved issue with "The bean 'pagedResourcesAssembler', defined in class path resource [com/teipsum/catalogservice/config/PaginationConfig.class], could not be registered. A bean with that name has already been defined in class path resource [org/springframework/data/web/config/HateoasAwareSpringDataWebConfiguration.class] and overriding is disabled."
    Logs from container.

--- backend/catalog-service/src/main/java/com/teipsum/catalogservice/config/PaginationConfig.java
+++ backend/catalog-service/src/main/java/com/teipsum/catalogservice/config/PaginationConfig.java
@@ -1,16 +1,16 @@
-package com.teipsum.catalogservice.config;
-
-import com.teipsum.catalogservice.model.CatalogProduct;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.web.PagedResourcesAssembler;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
-
-@Configuration
-@EnableSpringDataWebSupport
-public class PaginationConfig {
-    @Bean
-    public PagedResourcesAssembler<CatalogProduct> pagedResourcesAssembler() {
-        return new PagedResourcesAssembler<>(null, null);
-    }
-}
+//package com.teipsum.catalogservice.config;
+//
+//import com.teipsum.catalogservice.model.CatalogProduct;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.web.PagedResourcesAssembler;
+//import org.springframework.data.web.config.EnableSpringDataWebSupport;
+//
+//@Configuration
+//@EnableSpringDataWebSupport
+//public class PaginationConfig {
+//    @Bean
+//    public PagedResourcesAssembler<CatalogProduct> pagedResourcesAssembler() {
+//        return new PagedResourcesAssembler<>(null, null);
+//    }
+//}
