51467787822ebe7039de1074a7620900ee3311b8
Andrii Dovhal <dovhaland@gmail.com>
2025-07-29 18:56:18 +0200

    Final version without PrivateRoute. Auth is already checked in Header module. If user is authenticated - navigate to profile, otherwise - to login form.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,6 @@
-// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import { Provider } from 'react-redux';
 import { HelmetProvider } from 'react-helmet-async';
-// import store from './redux/store';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import HomePage from './pages/homePage/HomePage';
@@ -31,7 +28,6 @@ import TermsOfServicePage from './pages/TermsOfServicePage';
 import AccessibilityPage from './pages/AccessibilityPage';
 import CareersPage from './pages/CareersPage';
 import BlogPage from './pages/BlogPage';
-import PrivateRoute from './routes/PrivateRoute';
 import { ViewTypeProvider } from './context/ViewTypeContext';
 import { GenderProvider } from './context/GenderContext';
 import { HeaderHeightProvider } from './context/HeaderHeightContext';
@@ -42,59 +38,47 @@ import ScrollToTop from './components/ScrollToTop';
 function App() {
   return (
     <HelmetProvider>
-      {/* <Provider store={store}> */}
-        <ThemeProvider>
-          <LanguageProvider>
-            <HeaderHeightProvider>
-              <GenderProvider>
-                <ViewTypeProvider>
-                  {/* <Router> */}
-                    <ScrollToTop />
-                    {/* <div className="App"> */}
-                      <Header />
-                      <Routes>
-                        <Route path="/" element={<HomePage />} />
-                        <Route path="/pre-store" element={<PreStorePage />} />
-                        <Route path="/store" element={<StorePage />} />
-                        <Route path="/about" element={<AboutPage />} />
-                        <Route path="/contact" element={<ContactPage />} />
-                        <Route path="/login" element={<LoginPage />} />
-                        <Route path="/register" element={<RegisterPage />} />
-                        <Route path="/product/:id" element={<SingleProductPage />} />
-                        <Route path="/discounts" element={<DiscountsPage />} />
-                        <Route path="/new-collection" element={<NewCollectionPage />} />
-                        <Route path="/new-arrivals" element={<NewCollectionPage />} />
-                        <Route path="/checkout" element={<CheckoutPage />} />
-                        <Route path="/order-success" element={<OrderSuccessPage />} />
-                        <Route path="/shipping" element={<ShippingPage />} />
-                        <Route path="/returns" element={<ReturnsPage />} />
-                        <Route path="/cookies" element={<CookiePolicyPage />} />
-                        <Route path="/faq" element={<FAQPage />} />
-                        <Route path="/bestsellers" element={<BestsellersPage />} />
-                        <Route path="/size-guide" element={<SizeGuidePage />} />
-                        <Route path="/sustainability" element={<SustainabilityPage />} />
-                        <Route path="/accessibility" element={<AccessibilityPage />} />
-                        <Route path="/privacy" element={<PrivacyPolicyPage />} />
-                        <Route path="/terms" element={<TermsOfServicePage />} />
-                        <Route path="/careers" element={<CareersPage />} />
-                        <Route path="/blog" element={<BlogPage />} />
-                        {/* <Route 
-                          path="/profile" 
-                          element={
-                              <PrivateRoute element={<ProfilePage />} />
-                          } 
-                        /> */}
-                        <Route path="/profile" element={<ProfilePage />} />
-                      </Routes>
-                      <Footer />
-                    {/* </div>
-                  </Router> */}
-                </ViewTypeProvider>
-              </GenderProvider>
-            </HeaderHeightProvider>
-          </LanguageProvider>
-        </ThemeProvider>
-      {/* </Provider> */}
+      <ThemeProvider>
+        <LanguageProvider>
+          <HeaderHeightProvider>
+            <GenderProvider>
+              <ViewTypeProvider>
+                <ScrollToTop />
+                  <Header />
+                  <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/pre-store" element={<PreStorePage />} />
+                    <Route path="/store" element={<StorePage />} />
+                    <Route path="/about" element={<AboutPage />} />
+                    <Route path="/contact" element={<ContactPage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/register" element={<RegisterPage />} />
+                    <Route path="/product/:id" element={<SingleProductPage />} />
+                    <Route path="/discounts" element={<DiscountsPage />} />
+                    <Route path="/new-collection" element={<NewCollectionPage />} />
+                    <Route path="/new-arrivals" element={<NewCollectionPage />} />
+                    <Route path="/checkout" element={<CheckoutPage />} />
+                    <Route path="/order-success" element={<OrderSuccessPage />} />
+                    <Route path="/shipping" element={<ShippingPage />} />
+                    <Route path="/returns" element={<ReturnsPage />} />
+                    <Route path="/cookies" element={<CookiePolicyPage />} />
+                    <Route path="/faq" element={<FAQPage />} />
+                    <Route path="/bestsellers" element={<BestsellersPage />} />
+                    <Route path="/size-guide" element={<SizeGuidePage />} />
+                    <Route path="/sustainability" element={<SustainabilityPage />} />
+                    <Route path="/accessibility" element={<AccessibilityPage />} />
+                    <Route path="/privacy" element={<PrivacyPolicyPage />} />
+                    <Route path="/terms" element={<TermsOfServicePage />} />
+                    <Route path="/careers" element={<CareersPage />} />
+                    <Route path="/blog" element={<BlogPage />} />
+                    <Route path="/profile" element={<ProfilePage />} />
+                  </Routes>
+                  <Footer />
+              </ViewTypeProvider>
+            </GenderProvider>
+          </HeaderHeightProvider>
+        </LanguageProvider>
+      </ThemeProvider>
     </HelmetProvider>
   );
 }
