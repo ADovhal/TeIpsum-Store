c16a974fd39e21adb2d3b822dadaa9165d322356
Andrii Dovhal <dovhaland@gmail.com>
2025-07-29 18:01:01 +0200

    Should load profileData directly from the server. Solution for inifinite "Loading profile..."

--- frontend/src/routes/PrivateRoute.js
+++ frontend/src/routes/PrivateRoute.js
@@ -4,11 +4,5 @@ import { useSelector } from 'react-redux';
 
 export default function PrivateRoute({ element: Element }) {
   const isAuth = useSelector(s => s.auth.isAuthenticated);
-  const profile = useSelector(s => s.profile.profileData);
-  const loadingProfile = useSelector(s => s.profile.isLoading);
-
-  if (!isAuth) return <Navigate to="/login" />;
-  if (loadingProfile || profile === null) return <div>Loading profile…</div>;
-
-  return <Element />;
+  return isAuth ? <Element /> : <Navigate to="/login" />;
 }
