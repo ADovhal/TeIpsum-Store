a265533b9571b356d2184ecab0165e76f69c0cce
Andrii Dovhal <dovhaland@gmail.com>
2025-07-29 18:43:10 +0200

    Idk

--- frontend/src/routes/PrivateRoute.js
+++ frontend/src/routes/PrivateRoute.js
@@ -3,11 +3,11 @@ import { useSelector } from 'react-redux';
 
 export default function PrivateRoute({ element: Element }) {
   const isAuth = useSelector(s => s.auth.isAuthenticated);
-  const profile = useSelector(s => s.profile.profileData);
-  const loadingProfile = useSelector(s => s.profile.isLoading);
+//   const profile = useSelector(s => s.profile.profileData);
+//   const loadingProfile = useSelector(s => s.profile.isLoading);
 
   if (!isAuth) return <Navigate to="/login" />;
-  if (loadingProfile || profile === null) return <div>Loading profile…</div>;
+//   if (loadingProfile || profile === null) return <div>Loading profile…</div>;
 
   return <Element />;
 }
