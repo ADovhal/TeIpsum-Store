ae8824970f878d37842cfa030fe2f3a5522a18ce
Andrii Dovhal <dovhaland@gmail.com>
2025-07-29 15:33:32 +0200

    Now injecting store in apiUser, trying to go out of the loop.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,8 +4,11 @@ import 'font-awesome/css/font-awesome.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
+import { injectStore } from './services/apiUser';
 import store from './redux/store';
 import { BrowserRouter as Router } from 'react-router-dom';
+
+injectStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- frontend/src/services/apiUser.js
+++ frontend/src/services/apiUser.js
@@ -1,7 +1,13 @@
 import axios from 'axios';
 import apiAuth from './apiAuth';
-import store from '../redux/store';
+// import store from '../redux/store';
 import { setAccessToken, logoutAsync } from '../features/auth/authSlice';
+
+let appStore;
+
+export const injectStore = (_store) => {
+  appStore = _store;
+};
 
 const apiUser = axios.create({
   // baseURL: 'http://localhost:22080/api',
@@ -15,7 +21,7 @@ const apiUser = axios.create({
 apiUser.interceptors.request.use(
   (config) => {
     // const store = require('../redux/store').default;
-    const token = store.getState().auth.accessToken;
+    const token = appStore?.getState().auth.accessToken;
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
@@ -37,14 +43,14 @@ apiUser.interceptors.response.use(
         const newAccessToken = res.data.accessToken;
         // const store = require('../redux/store').default;
         // store.dispatch({ type: 'auth/setAccessToken', payload: newAccessToken });
-        store.dispatch(setAccessToken(newAccessToken));
+        appStore?.dispatch(setAccessToken(newAccessToken));
 
         originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
         return apiUser(originalRequest);
       } catch (refreshError) {
         // const store = require('../redux/store').default;
         // store.dispatch(require('../features/auth/authSlice').logoutAsync());
-        store.dispatch(logoutAsync());
+        appStore?.dispatch(logoutAsync());
         return Promise.reject(refreshError);
       }
     }
