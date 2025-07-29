c032b9dc24416aaf98c911cabba9c343ad908a09
Andrii Dovhal <dovhaland@gmail.com>
2025-07-29 18:16:32 +0200

    Testing without PrivateRoute again.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
+// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { Provider } from 'react-redux';
 import { HelmetProvider } from 'react-helmet-async';
+// import store from './redux/store';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import HomePage from './pages/homePage/HomePage';
@@ -39,12 +42,15 @@ import ScrollToTop from './components/ScrollToTop';
 function App() {
   return (
     <HelmetProvider>
+      {/* <Provider store={store}> */}
         <ThemeProvider>
           <LanguageProvider>
             <HeaderHeightProvider>
               <GenderProvider>
                 <ViewTypeProvider>
+                  {/* <Router> */}
                     <ScrollToTop />
+                    {/* <div className="App"> */}
                       <Header />
                       <Routes>
                         <Route path="/" element={<HomePage />} />
@@ -72,19 +78,23 @@ function App() {
                         <Route path="/terms" element={<TermsOfServicePage />} />
                         <Route path="/careers" element={<CareersPage />} />
                         <Route path="/blog" element={<BlogPage />} />
-                        <Route 
+                        {/* <Route 
                           path="/profile" 
                           element={
                               <PrivateRoute element={<ProfilePage />} />
                           } 
-                        />
+                        /> */}
+                        <Route path="/profile" element={<ProfilePage />} />
                       </Routes>
                       <Footer />
+                    {/* </div>
+                  </Router> */}
                 </ViewTypeProvider>
               </GenderProvider>
             </HeaderHeightProvider>
           </LanguageProvider>
         </ThemeProvider>
+      {/* </Provider> */}
     </HelmetProvider>
   );
 }
