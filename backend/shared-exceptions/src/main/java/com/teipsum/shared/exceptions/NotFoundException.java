2baaf515f72519cd68806d9e3a0261964ed9247d
Andrii Dovhal <dovhaland@gmail.com>
2025-07-31 03:41:29 +0200

    Added new exceptions to shared-exceptions. Changed price to priceSnapshot in OrderLineItem (shared-product-dto).

---
+++ backend/shared-exceptions/src/main/java/com/teipsum/shared/exceptions/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.teipsum.shared.exceptions;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
