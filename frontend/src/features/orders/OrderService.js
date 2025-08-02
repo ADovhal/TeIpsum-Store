fcca0bb7ac75e62f1f493bf13f78eb6e92ca5db8
Andrii Dovhal <dovhaland@gmail.com>
2025-08-02 20:58:36 +0200

    Correct endpoint used to load user's orders.

--- frontend/src/features/orders/OrderService.js
+++ frontend/src/features/orders/OrderService.js
@@ -1,4 +1,4 @@
 import apiOrder from '../../services/apiOrder';
 
-export const getMyOrders = () => apiOrder.get('/orders/user').then(res => res.data.orders || []);
+export const getMyOrders = () => apiOrder.get('/orders/my').then(res => res.data.orders || []);
 export const getOrderById = (orderId) => apiOrder.get(`/orders/${orderId}`).then(res => res.data);
