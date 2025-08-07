1731e13ae997978bbbe8f4d03a6c2b261a4c3cd3
Andrii Dovhal <dovhaland@gmail.com>
2025-08-07 16:54:25 +0200

    Changes in collect-spec script to store all api-docs in one container (one folder). Now is used localhost of server instead of names of containers, which were unreachable from script (Docker DNS).

--- docs/collect-specs.sh
+++ docs/collect-specs.sh
@@ -2,20 +2,21 @@
 set -e
 mkdir -p specs
 
-services=(
-  "auth:9090"
-  "user:9093"
-  "catalog:9098"
-  "order:8100"
-  "admin-product:9096"
+declare -A ports=(
+  [auth]=22093
+  [user]=22094
+  [catalog]=22096
+  [order]=22001
+  [admin-product]=22095
 )
 
-for svc in "${services[@]}"; do
-  IFS=':' read -r name port <<< "$svc"
+for name in "${!ports[@]}"; do
+  port=${ports[$name]}
+  url="http://127.0.0.1:${port}/v3/api-docs"
   echo "🔗 Checking $name on port $port..."
-  if curl -sSf http://prod-$name:$port/v3/api-docs -o /dev/null; then
+  if curl -sSf "$url" >/dev/null; then
     echo "✅ $name is available"
-    curl -sSf http://prod-$name:$port/v3/api-docs > specs/${name}.json
+    curl -sSf "$url" > "specs/${name}.json"
   else
     echo "❌ $name is not available, skipping..."
   fi
