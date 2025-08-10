5e83a39080fa798291d37722e8b4976952656d41
Andrii Dovhal <dovhaland@gmail.com>
2025-08-10 19:07:16 +0200

    Catalog-service. Added @OpenAPIDefinition annotation to main class. Also removed Dockerfile of docs. Changed nginx.prod.conf. Added new location /api-docs/.

--- backend/catalog-service/src/main/java/com/teipsum/catalogservice/CatalogServiceApplication.java
+++ backend/catalog-service/src/main/java/com/teipsum/catalogservice/CatalogServiceApplication.java
@@ -1,8 +1,18 @@
 package com.teipsum.catalogservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Catalog Service",
+        version = "1.0.0",
+        description = "TeIpsum Catalog Service"
+    )
+)
 @SpringBootApplication
 public class CatalogServiceApplication {
 
