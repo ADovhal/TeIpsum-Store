87ec3f8a5d1e58b04fb834c59c1e2b94b1ae4411
Andrii Dovhal <dovhaland@gmail.com>
2025-08-10 17:14:38 +0200

    Changed way to take all the logs to one container. Changed script collect-specs.sh. Trying new application.yml configuration for the catalog-service.

--- backend/catalog-service/src/main/java/com/teipsum/catalogservice/config/SecurityConfig.java
+++ backend/catalog-service/src/main/java/com/teipsum/catalogservice/config/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/products/v3/api-docs/**", "/api/products/swagger-ui/**", "/api/products/swagger-ui.html").permitAll()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .anyRequest().permitAll()
                 );
         return http.build();
--- docs/collect-specs.sh
+++ docs/collect-specs.sh
@@ -12,18 +12,29 @@ declare -A ports=(
 
 for svc in "${!ports[@]}"; do
   port=${ports[$svc]}
-  url="http://127.0.0.1:${port}/v3/api-docs"
+  url="http://localhost:${port}/v3/api-docs"
   echo "🔗 Checking $svc on port $port..."
 
   for i in {1..10}; do
     if curl -sSf "$url" >/dev/null; then
       echo "✅ $svc is available"
-      curl -sSf "$url" \
-        | jq --arg url "./${svc}.json" '.servers[0].url = $url' \
-        > "specs/${svc}.json"
+      curl -sSf "$url" > "specs/${svc}.json"
       break
     fi
     echo "⏳ Waiting $svc ($i/10)..."
     sleep 3
   done || echo "❌ $svc unavailable after 30 s"
 done
+
+# Generate swagger-config.json
+cat <<EOF > specs/swagger-config.json
+{
+  "urls": [
+    {"url": "/api-docs/auth.json", "name": "Auth Service"},
+    {"url": "/api-docs/user.json", "name": "User Service"},
+    {"url": "/api-docs/catalog.json", "name": "Catalog Service"},
+    {"url": "/api-docs/order.json", "name": "Order Service"},
+    {"url": "/api-docs/admin-product.json", "name": "Admin Product Service"}
+  ]
+}
+EOF
