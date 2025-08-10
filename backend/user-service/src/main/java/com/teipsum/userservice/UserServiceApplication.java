551adc8d66b3158acb60a7f08024b9835570626d
Andrii Dovhal <dovhaland@gmail.com>
2025-08-10 19:28:49 +0200

    User-service. Added @OpenAPIDefinition annotation to main class.

--- backend/user-service/src/main/java/com/teipsum/userservice/UserServiceApplication.java
+++ backend/user-service/src/main/java/com/teipsum/userservice/UserServiceApplication.java
@@ -1,8 +1,18 @@
 package com.teipsum.userservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@OpenAPIDefinition(
+    info = @Info(
+        title = "User Service",          
+        version = "1.0.0",               
+        description = "TeIpsum User Management Service"
+    )
+)
 @SpringBootApplication
 public class UserServiceApplication {
 
